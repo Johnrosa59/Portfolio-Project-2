ddb2e11922c79ba22fa2228447a6daf33af994f3
Johnrosa59 <jvrosatecnologia@gmail.com>
2023-11-24 15:56:52 -0300

    Merge branch 'main' of https://github.com/Johnrosa59/Portfolio-Project-2
    undefined
    
    undefined
    
    undefined

---
+++ README.md
@@ -0,0 +1,32 @@
+<h1>Portfólio Project</h1>
+<br>
+<br>
+<h2>Esse é o site do meu portfólio os projetos foram feitos por mim</h2>
+  
+  <h2>Sobre o Projeto</h2>
+  
+  Foram utilizados a seguintes técnologias<br>
+  
+  -HTML<br>
+  -CSS<br>
+  -Javascript<br>
+  
+  O layout do site foi pego na internet, achei bonito e legal para usar em meu portifólio.
+  
+  Nesse site usei as tag semânticas header, main e footer, sections. Usei div também.
+  
+  Fiz apenas com essas duas técnologias(HTML e CSS) conforme meu aprendizado for aumentando pretendo aplicar outras técnologias.
+  
+  Para mim não foi díficil tive facilidade em aplicar o código aprendido.
+  
+<img src="https://github.com/Johnrosa59/Portfolio-Project-2/blob/main/img/README.png">
+<img src="https://github.com/Johnrosa59/Portfolio-Project-2/blob/main/img/README2.png">
+<img src="https://github.com/Johnrosa59/Portfolio-Project-2/blob/main/img/README3.png">
+<img src="https://github.com/Johnrosa59/Portfolio-Project-2/blob/main/img/README4.png">
+<br>
+<br>
+Versão Mobile do site:
+
+<img src="https://github.com/Johnrosa59/Portfolio-Project-2/blob/main/img/README-Mobile.png">
+<img src="https://github.com/Johnrosa59/Portfolio-Project-2/blob/main/img/README-Mobile-2.png">
+<img src="https://github.com/Johnrosa59/Portfolio-Project-2/blob/main/img/README-Mobile-3.png">
---
+++ scripts.js
@@ -0,0 +1,66 @@
+const buttonOne = document.getElementById("button-1");
+const siteImg = document.getElementById("sites-img");
+const link1 = document.getElementById("link1");
+const buttonTwo = document.getElementById("button-2");
+const buttonThree = document.getElementById("button-3");
+const buttonFour = document.getElementById("button-4");
+const buttonFive = document.getElementById("button-5");
+const buttonSix = document.getElementById("button-6");
+const buttonSeven = document.getElementById("button-7");
+const buttonEight = document.getElementById("button-8");
+
+const changeImgLink1 = () => {
+  siteImg.src = "./img/2-site-2.png";
+  link1.href = "https://johnrosa59.github.io/Projeto-page-green/";
+};
+
+const changeImgLink2 = () => {
+  siteImg.src = "./img/1-site-1.png";
+  link1.href = "https://johnrosa59.github.io/Projeto-Shopping-Mobile/";
+};
+
+const changeImgLink3 = () => {
+  siteImg.src = "./img/3-site-3.png";
+  link1.href = "https://johnrosa59.github.io/We-Care-Project/";
+};
+
+const changeImgLink4 = () => {
+  siteImg.src = "./img/4-site-4.png";
+  link1.href = "https://johnrosa59.github.io/Login-Menu-Project/";
+};
+
+const changeImgLink5 = () => {
+  siteImg.src = "./img/5-site-5.png";
+  link1.href = "https://johnrosa59.github.io/Project-Perfil/";
+};
+
+const changeImgLink6 = () => {
+  siteImg.src = "./img/6-site-6.png";
+  link1.href = "https://johnrosa59.github.io/UI-KIT-Layout/";
+};
+
+const changeImgLink7 = () => {
+  siteImg.src = "./img/7-site-7.png";
+  link1.href = "https://johnrosa59.github.io/Convert-money-I-do/";
+};
+
+const changeImgLink8 = () => {
+  siteImg.src = "./img/todolist.png";
+  link1.href = "https://johnrosa59.github.io/Project-o-que-tenho-para-fazer/";
+};
+
+buttonOne.addEventListener("click", changeImgLink1);
+
+buttonTwo.addEventListener("click", changeImgLink2);
+
+buttonThree.addEventListener("click", changeImgLink3);
+
+buttonFour.addEventListener("click", changeImgLink4);
+
+buttonFive.addEventListener("click", changeImgLink5);
+
+buttonSix.addEventListener("click", changeImgLink6);
+
+buttonSeven.addEventListener("click", changeImgLink7);
+
+buttonEight.addEventListener("click", changeImgLink8);
